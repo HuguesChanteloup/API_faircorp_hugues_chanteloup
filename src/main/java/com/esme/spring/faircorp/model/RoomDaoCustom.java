c362f99cd453fa7962923cacf7d29e81566a9023
Hugues Chanteloup <hugues.chanteloup@etu.emse.com>
2019-03-07 19:25:39 +0100

    debug RoomDaoCustom

--- src/main/java/com/esme/spring/faircorp/model/RoomDaoCustom.java
+++ src/main/java/com/esme/spring/faircorp/model/RoomDaoCustom.java
@@ -1,5 +1,7 @@
 package com.esme.spring.faircorp.model;
 
+import java.util.List;
+
 public interface RoomDaoCustom {
     Room FindRoomByName(String name);
     List<Room> FindRoomByBuilding(Building building);
