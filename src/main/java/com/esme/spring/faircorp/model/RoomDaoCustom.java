b1ad3a731e8a685b8ef6b470833f36dcf77ced6d
Hugues Chanteloup <hugues.chanteloup@etu.emse.com>
2019-03-10 21:38:03 +0100

    Class error corrected

--- src/main/java/com/esme/spring/faircorp/model/RoomDaoCustom.java
+++ src/main/java/com/esme/spring/faircorp/model/RoomDaoCustom.java
@@ -3,6 +3,6 @@ package com.esme.spring.faircorp.model;
 import java.util.List;
 
 public interface RoomDaoCustom {
-    List<Room> FindRoomByName(String name);
+    Room FindRoomByName(String name);
     List<Room> FindRoomByBuilding(Building building);
 }
